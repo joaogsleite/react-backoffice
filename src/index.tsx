76e7f6eee98a151c13154cca96a42514bcd35758
joaogsleite <joao.leite@isobar.com>
2020-05-13 11:32:21 +0100

    index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 import RootView from 'views/Root';
 import { Provider } from './reducers';
